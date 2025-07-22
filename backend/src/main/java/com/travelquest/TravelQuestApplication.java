d72b02dda02edb3b36bcc556b5e0e3d13e8b7b76
DevikaDixit <devika16dixit2000@gmail.com>
2025-07-22 22:53:10 +0530

    TravelQuest: Initial backend repository

---
+++ backend/src/main/java/com/travelquest/TravelQuestApplication.java
@@ -0,0 +1,13 @@
+package com.travelquest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelQuestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelQuestApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/travelquest/TravelQuestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelquest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelQuestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
